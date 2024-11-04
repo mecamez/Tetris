0f10d27249dbacb855527e73713fe9ee0178ce50
mecamez <165615674+mecamez@users.noreply.github.com>
2024-11-03 22:14:49 -0700

    Update GameScene.swift

--- Tetris/GameScene.swift
+++ Tetris/GameScene.swift
@@ -11,15 +11,39 @@ class GameScene: SKScene {
         addChild(char)
     }
 
-    override func keyDown(with event: NSEvent) {
-        switch event.charsIgnoringModifiers {
-        case "w":
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create tap gesture recognizers for each direction
+        let upTap = NSClickGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let downTap = NSClickGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let leftTap = NSClickGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let rightTap = NSClickGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+
+        // Set identifiers or tags for direction identification
+        upTap.name = "up"
+        downTap.name = "down"
+        leftTap.name = "left"
+        rightTap.name = "right"
+
+        // Add gesture recognizers to respective zones or views
+        upZone.addGestureRecognizer(upTap)
+        downZone.addGestureRecognizer(downTap)
+        leftZone.addGestureRecognizer(leftTap)
+        rightZone.addGestureRecognizer(rightTap)
+    }
+    
+    @obje func handleTap(_ sender: NSClickGestureRecognizer) {
+        guard let direction = sender.name else { return }
+
+        switch direction {
+        case "up":
             char.move(direction: .up)
-        case "s":
+        case "down":
             char.move(direction: .down)
-        case "a":
+        case "left":
             char.move(direction: .left)
-        case "d":
+        case "right":
             char.move(direction: .right)
         default:
             break
